e97d42c8a090184864501a2b45a0f0903dd7b5ff
Jadwiga Bochinska <matuzalema@gmail.com>
2019-01-02 19:18:36 +0100

    Change document-write to insertAndjacentHTML

--- js/script.js
+++ js/script.js
@@ -4,19 +4,21 @@ function Phone(brand, price, color, memory){
 	this.color = color;
 	this.memory = memory;
 }
+	var phonesField = document.getElementById('phones');
+
 Phone.prototype.printInfo = function(){
-	document.write("Phone: " + this.brand + "<br>" + "Color: " + this.color +  "<br>" +"Memory: " + this.memory + "<br>" + "Price: " + this.price + " zł" + "<br>" + "<br>");
+	phones.insertAdjacentHTML("afterbegin", "<div>" + "Phone: " + this.brand + "<br>" + "Color: " + this.color +  "<br>" +"Memory: " + this.memory + "<br>" + "Price: " + this.price + " zł" + "<br>" + "<br>" + "</div>");
 };
 
 Phone.prototype.sale = function(){
-	document.write("SALE" + "<br>");
+	phonesField.insertAdjacentHTML("afterbegin", "SALE" + "<br>");
 };
 
 const SamsungGalaxyS6 = new Phone ('Apple', 2250, 'silver', "2 GB");
-SamsungGalaxyS6.sale();
 SamsungGalaxyS6.printInfo();
+SamsungGalaxyS6.sale();
 const GalaxyS7 = new Phone ('Samsung', 1999, 'silver', "4 GB");
 GalaxyS7.printInfo();
 const HTC = new Phone ('HTC', 1200, 'black', "4 GB");
-HTC.sale();
 HTC.printInfo();
+HTC.sale();
